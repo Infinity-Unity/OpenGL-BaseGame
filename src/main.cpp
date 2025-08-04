16b3bfed5be5e3839f898d21135034896728e169
Abdul <thbgstudios@gmail.com>
2025-08-05 00:13:58 +0300

    Fix Errors

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,19 @@
 #include <GLFW/glfw3.h>
 #include "iostream"
 
+extern "C"
+{
+    _declspec(dllexport) unsigned long NvOptimusEnablement = 0x00000001; // select the  nvidia driver
+}
+
+
+void Display()
+{
+    //glClearColor();
+}
+
+const int WIDTH = 800;
+const int HEIGHT = 600;
 
 int main(void)
 {
@@ -13,20 +26,26 @@ int main(void)
         return -1;
     }
 
+
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    
+
 
     GLFWwindow* window;
+    window = glfwCreateWindow(WIDTH, HEIGHT, "Game", nullptr, nullptr);
+    
 
-    window = glfwCreateWindow(640, 480, "Game", nullptr, nullptr);
     if (!window)
     {
         glfwTerminate();
         return -1;
     }
 
+
     glfwMakeContextCurrent(window);
+    
 
     if (!gladLoadGL())
     {
@@ -37,19 +56,14 @@ int main(void)
     std::cout << "Renderer: " << glGetString(GL_RENDERER) << "\n";
     std::cout << "OpenGL Version: " << glGetString(GL_VERSION) << "\n";
    
-
-
-    glClearColor(2, 3, 5, 0);
-
+    glClearColor(0.1f,0.2f,0.2f,0);
     
+
     while (!glfwWindowShouldClose(window))
     {
         glClear(GL_COLOR_BUFFER_BIT);
-
-
+        glViewport(5, 5, 800, 600);
         glfwSwapBuffers(window);
-
-
         glfwPollEvents();
     }
 
